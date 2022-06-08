09513eccbcbdc14f792ee0f3b5d131629bd65923
aimunir <aimunir25@outlook.com>
2022-06-08 16:23:30 +0600

    remove button added

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from '../TOdo/Todo'
 import TodoForm from '../TodoForm/TodoForm'
 
 function TodoList() {
@@ -14,10 +15,28 @@ function TodoList() {
         console.log(newTodos);
     }
 
+    const removeTodo=id=>{
+        const removeArr=[...todos].filter(todo=>todo.id !==id)
+        setTodos(removeArr);
+    };
+
+
+
+    const completeTodo=id=>{
+        let updateTodos=todos.map(todo=>{
+            if(todo.id===id){
+                todo.isComplete=!todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updateTodos);
+    };
+
   return (
     <div>
         <h1>What's Plan For Today</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}/>
     </div>
   )
 }
